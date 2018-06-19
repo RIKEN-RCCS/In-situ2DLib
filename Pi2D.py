a70c1f22630690683c03c28c93e80a2661ca2c34
Tozaki Chisae <tozaki_chisae@jp.fujitsu.com>
2018-06-19 18:03:26 +0900

    [WIP] add 1 file and modify 2 files.

---
+++ Pi2D.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Pi2D:
+
+  def __init__(self):
+    print("python done")
+
+  def DrawS(self, mid):
+    fig = plt.Figure(mid);
+
+    return True
+
+  def DrawV(self):
+    return True
+
+  def Output(self):
+    return True
+
