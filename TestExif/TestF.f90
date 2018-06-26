d40d5dc888943f79ab9594850d2edc92ab7f653f
yoh <yoh@jp.fujitsu.com>
2018-06-26 18:15:09 +0900

    add TestExif

---
+++ TestExif/TestF.f90
@@ -0,0 +1,24 @@
+program testf
+  implicit none
+  include 'pi2d_f.inc'
+  real*4 :: data(80, 60)
+  real*4 :: pi = 3.14159
+  integer :: i, j, iret
+  character*128 :: atrarg, lutn
+
+  do j = 1, 60
+     do i = 1, 80
+        data(i, j) = (i * 2d0 * pi / 79) * (j * 2d0 * pi / 59)
+     enddo
+  enddo
+
+  iret = pi2d_init()
+  atrarg = "arraySize=[60,80]"
+  iret = pi2d_setattrib(atrarg, 17)
+  lutn = "default"
+  iret = pi2d_draws(0, data, lutn, 7, 10, 0)
+  iret = pi2d_output(0, 0, 0, 0)
+  iret = pi2d_finalize()
+  
+  stop
+end program testf
